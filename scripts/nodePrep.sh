89cda3fcfc4fcee380353f63c4e7b1142a22d92f
Alexanderdw <alexander_de_wispelaere@hotmail.com>
2019-01-31 16:05:49 +0100

    Update nodePrep.sh

--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -70,7 +70,7 @@ subscription-manager repos --disable="*"
 
 echo $(date) " - Enabling only the required repos"
 
-subscription-manager repos \
+sudo subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
     --enable="rhel-7-server-ose-3.11-rpms" \
